5ac14c5c6cad87b82905685c26f38e7d29eb8466
TejashwiniPulipati <nehateja41@gmail.com>
2025-03-05 19:44:12 +0530

    creating public route table and creating routes to igw , web , api subnets.

--- tf-aws/vpc.tf
+++ tf-aws/vpc.tf
@@ -49,3 +49,27 @@ resource "aws_internet_gateway" "igw" {
     Name = "lms-igw"
   }
 }
+
+# public-route-table
+resource "aws_route_table" "lms-pub-rt" {
+  vpc_id = aws_vpc.lms-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "aws_internet_gateway.igw.id"
+  }
+  tags = {
+    Name = "lms-public-rt"
+  }
+}
+
+# web-subnet-association
+resource "aws_route_table_association" "web-sn-asc" {
+  subnet_id      = aws_subnet.lms-web-sn.id
+  route_table_id = aws_route_table.lms-pub-rt.id
+}
+
+# api-subnet-association
+resource "aws_route_table_association" "api-sn-asc" {
+  subnet_id      = aws_subnet.lms-api-sn.id
+  route_table_id = aws_route_table.lms-pub-rt.id
+}
