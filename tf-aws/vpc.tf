d29cbc46a2a00a53af5d008276a127687b39c161
TejashwiniPulipati <nehateja41@gmail.com>
2025-03-05 20:20:36 +0530

    corrected cidr block it should be in list format

--- tf-aws/vpc.tf
+++ tf-aws/vpc.tf
@@ -145,7 +145,7 @@ resource "aws_security_group" "lms-web-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -153,12 +153,12 @@ resource "aws_security_group" "lms-web-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     protocol    = "-1"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
