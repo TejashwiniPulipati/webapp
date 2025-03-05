671aaa9b06f03760c5ffb002a8ced824ec3dbcd6
TejashwiniPulipati <nehateja41@gmail.com>
2025-03-05 17:35:55 +0530

    creating variables file

---
+++ tf-aws/variables.tf
@@ -0,0 +1,11 @@
+# Access Key
+variable "aws_access_key" {
+  type        = string
+  description = "enter aws access key"
+}
+
+# Secret Key
+variable "aws_secret_key" {
+  type        = string
+  description = "enter aws secret key"
+}
